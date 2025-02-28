9482a18bdd8a3488bf72a167eceefbf681e59d04
Joao Victor <devjoaovictor10@gmail.com>
2025-02-28 13:18:55 -0300

    feat: criando tela de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,35 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import {
+  Card,
+  CardTitle,
+  CardHeader,
+  CardContent,
+  CardDescription,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div></div>;
+  const route = useRouter();
+
+  const handleLogin = () => route.push("/clients");
+
+  return (
+    <div className="mx-auto flex h-screen w-full items-center justify-center">
+      <Card className="mx-4 flex size-[320px] flex-col justify-between">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Faça seu login com o google</CardDescription>
+        </CardHeader>
+
+        <CardContent className="flex flex-1 items-center justify-center">
+          <Button className="w-full" onClick={handleLogin}>
+            Login
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
