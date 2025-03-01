50a138edd871b8d02ca30cdf810e63cfebfe0ffb
Joao Victor <devjoaovictor10@gmail.com>
2025-03-01 15:18:55 -0300

    feat: configuracao inicial do prisma e nextAuth

---
+++ src/lib/auth.ts
@@ -0,0 +1,16 @@
+import { AuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+
+import { db } from "./prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_AUTH_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_AUTH_CLIENT_SECRET as string,
+    }),
+  ],
+};
