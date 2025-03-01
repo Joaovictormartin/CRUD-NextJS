4284e271173196b95faf30c033f06027097a37b3
Joao Victor <devjoaovictor10@gmail.com>
2025-03-01 14:56:18 -0300

    modal de confirmacao de exclusao

---
+++ src/app/(admin)/clients/components/modal-confirm-student-exclusion.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Trash2 } from "lucide-react";
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogHeader,
+  DialogFooter,
+  DialogContent,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+interface ModalConfirmStudentExclusionProps {
+  open: boolean;
+  client: any;
+  setOpen: (open: boolean) => void;
+}
+
+export function ModalConfirmStudentExclusion({
+  open,
+  client,
+  setOpen,
+}: ModalConfirmStudentExclusionProps) {
+  const names = client?.map((item: any) => item.name)?.join(", ");
+
+  const handleDeleteClient = () => {};
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="max-w-[420px]">
+        <DialogHeader>
+          <DialogTitle className="text-sm font-bold">
+            <strong className="text-[#F87171]">CUIDADO:</strong> Você está
+            prestes a excluir um cliente!
+          </DialogTitle>
+        </DialogHeader>
+
+        <DialogDescription className="my-0 border-y border-[#94A3B8] py-6">
+          Tem certeza de que deseja excluir permanentemente o cliente(s) {names}
+          ? Esta ação não pode ser desfeita e todos os dados relacionados ao
+          cliente(s), incluindo histórico de empréstimos e faturas, serão
+          removidos permanentemente.
+        </DialogDescription>
+
+        <DialogFooter className="flex justify-end gap-2">
+          <Button onClick={() => setOpen(false)}>Cancelar</Button>
+
+          <Button variant={"destructive"} onClick={handleDeleteClient}>
+            <Trash2 />
+            Deletar
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
