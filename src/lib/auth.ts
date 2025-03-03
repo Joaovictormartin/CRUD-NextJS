f98433c996900415b152a5b79582879b1bf164c0
Joao Victor <devjoaovictor10@gmail.com>
2025-03-02 21:16:09 -0300

    fix: adicionando id no session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,7 +5,14 @@ import GoogleProvider from "next-auth/providers/google";
 import { db } from "./prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
 export const authOptions: AuthOptions = {
+  secret: process.env.NEXT_AUTH_SECRET,
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
@@ -13,4 +20,11 @@ export const authOptions: AuthOptions = {
       clientSecret: process.env.NEXT_AUTH_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user = { ...session.user, id: user.id } as User;
+
+      return session;
+    },
+  },
 };
