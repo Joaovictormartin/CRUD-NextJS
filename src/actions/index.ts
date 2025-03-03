f95fccf309ca6a6cb86dcb8066a65a58725ec354
Joao Victor <devjoaovictor10@gmail.com>
2025-03-03 10:43:30 -0300

    feat: exoprt actions

---
+++ src/actions/index.ts
@@ -0,0 +1,5 @@
+import { getClients } from "./getClients";
+import { createClient } from "./createClient";
+import { deleteClients } from "./deleteClients";
+
+export { getClients, createClient, deleteClients };
