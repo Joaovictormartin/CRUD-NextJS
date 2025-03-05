fdbfe23ac3585dda18b0b71312976edeb030d8dc
Joao Victor <devjoaovictor10@gmail.com>
2025-03-05 10:12:21 -0300

    fix: correcao no getClients

--- src/actions/getClients.ts
+++ src/actions/getClients.ts
@@ -1,14 +1,22 @@
 "use server";
 
+import { getServerSession } from "next-auth";
+
 import { db } from "@/lib/prisma";
+import { authOptions } from "@/lib/auth";
 
 interface GetClientsProps {
   search: string;
 }
 
 export const getClients = async ({ search }: GetClientsProps) => {
+  const user = await getServerSession(authOptions);
+
+  if (!user) throw new Error("Usuário não autenticado");
+
   return db.client.findMany({
     where: {
+      userId: (user.user as any).id,
       OR: [
         { name: { contains: search, mode: "insensitive" } },
         { email: { contains: search, mode: "insensitive" } },
